57a4892540f2051ac7fa605f26059da0e0874d76
HamdiAhnafAswana <161789279+HamdiAhnafAswana@users.noreply.github.com>
2024-04-27 14:03:38 +0700

    Sumary

---
+++ Projek_Toko_Skincare/src/main/java/com/mycompany/projek_toko_skincare/Projek_Toko_Skincare.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projek_toko_skincare;
+
+/**
+ *
+ * @author HP
+ */
+public class Projek_Toko_Skincare {
+
+    public static void main(String[] args) {
+        new Toko_Skincare().show();
+    }
+}
